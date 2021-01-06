667a6d9cd7a99fe3190d10b84bde4abfeaa31301
Oskar <saturdaysheroes.business@gmail.com>
2021-01-06 17:34:15 +0100

    added /create route

---
+++ src/routes/create.ts
@@ -0,0 +1,37 @@
+import {Router} from "express";
+import {createLabel} from "../utils/label";
+
+const router = Router();
+
+export default function() {
+    router.post("/create", (req, res) => {
+        let body = req.body; 
+
+        if (!body || !body.sender || !body.recipient || !body.carrier || !body.address) {
+            res.json({
+                code: 400, 
+                message: "missing parameter"
+            });
+            res.status(400);
+            return; 
+        };
+
+        createLabel({
+            sender: body.sender, 
+            recipient: body.recipient, 
+            carrier: body.carrier, 
+            address: body.address,
+            notes: body.notes || ""
+        })
+        .then(id => {
+            if (id == "error") {
+                res.sendStatus(500);
+                return;
+            };
+
+            res.redirect("/download/" + id);
+        })
+    });
+
+    return router;
+};
